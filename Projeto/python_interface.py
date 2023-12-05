a8d746c3808b02788fc32afab6726efd6577f0cc
afp2 <afp2@g5c13.cin.ufpe.br>
2023-12-05 17:41:55 -0300

    prototipo alterado

--- Projeto/python_interface.py
+++ Projeto/python_interface.py
@@ -2,17 +2,30 @@ print('################################################')
 print('########## Bem vindo ao poCIntividade ##########')
 print('################################################')
 print('')
-print('Opcoes:')
-print('sair - fechar o programa')
-print('msg - enviar nova mensagem')
-print('')
+
+
+def confere_mensagem(mensagem):
+    permissao = input(f'A mensagem {mensagem} é permitida? (y/n) ')
+    if permissao == 'y':
+        return True
+    else:
+        return False
+
 
 while True:
+    print('Opcoes:')
+    print('sair - fechar o programa')
+    print('msg - enviar nova mensagem')
+    print('')
     decisao = input()
     if decisao == 'sair':
         break
     elif decisao == 'msg':
-        input('sua mensagem: ')
+        mensagem = input('sua mensagem: ')
+        if confere_mensagem(mensagem):
+            print(f'Mensagem enviada:\n{mensagem}')
+        else:
+            print('Mensagem proibida')
     else:
         print('opcao invalida')
     print('')
